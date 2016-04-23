2f292dbab14a60e966071ddf0830f3dcb0cfb217
Paul Serraino <pserraino99@gmail.com>
2016-04-23 11:38:21 -0500

    ramda auto curries

--- ramda.js
+++ ramda.js
@@ -2,17 +2,12 @@ var R = require('ramda')
 var reduceReducers = require('reduce-reducers')
 var Container = require('./ramda-container')
 
-var transduce = R.compose(
-    R.partial(R.transduce),
-    R.unapply(R.identity)
-)
-
-var increment = transduce(
+var increment = R.transduce(
     R.filter(a => a.type === 'INC'),
     R.compose(R.add(1))
 )
 
-var decrement = transduce(
+var decrement = R.transduce(
     R.filter(a => a.type === 'DEC'),
     R.compose(R.flip(R.subtract)(1))
 )
